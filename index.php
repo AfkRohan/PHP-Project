7bbb48f383d15275ba4071b3b5792df87a22d056
ForamP0706 <patelforam530@gmail.com>
2023-08-03 15:34:48 -0400

    foram

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ require_once("functions.inc.php");
 <!DOCTYPE html>
 <html>
 <head>
-    <!-- Test4 -->
+    <!-- Test -->
     <link rel="stylesheet" href="style.css">
     <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.2.1/css/fontawesome.min.css" rel="stylesheet">
@@ -46,135 +46,28 @@ require_once("functions.inc.php");
         </div>
     </div>
 </div>
-<!-- featured categories --->
-<div class="categories">
-    <div class="small-container">
-        <div class=" row">
-            <div class="col-4">
-                <img src="images/vegetables.jpeg">
-                <h4>VEGETABLES</h4>
-            </div>
-            <div class="col-4">
-                <img src="images/snacks.jpg">
-                <h4>SNACKS AND CHIPS</h4>
-            </div>
-            <div class="col-4">
-                <img src="images/Dairy.jpg">
-                <h4>DAIRY PRODUCTS</h4>
-            </div>
-            <div class="col-4">
-                <img src="images/pantry.jpeg">
-                <h4>PANTRY</h4>
-            </div>
-        </div>
-    </div>
+<?php require_once("db/db_conn.php"); ?>
+
+<div class="container">
+<div class="row text-center py-5">
+    <?php
+    global $pdo;
+    $stmt = $pdo->query("SELECT * FROM products");
+
+    while($row=$stmt->fetch()){
+        component($row['product_name'], $row['product_price'], $row['product_image'], $row['id']);
+    }
+
+   
 </div>
-<!-- featured products--->
-<div class="small-container">
-    <h2 class="title">Featured Products</h2>
-    <div class="row">
-        <div class="col-4">
-            <img src="images/1.png">
-            <h4>Basmati Rice</h4>
-            <p>$18.00</p>
-            <a href="cart.php" class="btn">Add to cart &#8594;</a>
-        </div>
-        <div class="col-4">
-            <img src="images/2.png">
-            <h4>Dumplings</h4>
-            <p>$10.00</p>
-            <a href="cart.php" class="btn">Add to cart &#8594;</a>
-        </div>
-        <div class="col-4">
-            <img src="images/3.png">
-            <h4>Tikka masala</h4>
-            <p>$20.00</p>
-            <a href="cart.php" class="btn">Add to cart &#8594;</a>
-        </div>
-        <div class="col-4">
-            <img src="images/4.png">
-            <h4>Gulab jamun Mix</h4>
-            <p>$15.00</p>
-            <a href="cart.php" class="btn">Add to cart &#8594;</a>
-        </div>
-    </div>
 </div>
-<!-- Latest products -->
-<div class="small-container">
-    <h2 class="title">Latest Products</h2>
-    <div class="row">
-        <div class="col-4">
-            <img src="images/5.png">
-            <h4>Basmati Rice</h4>
-            <p>$18.00</p>
-            <a href="cart.php" class="btn">Add to cart &#8594;</a>
-        </div>
-        <div class="col-4">
-            <img src="images/6.png">
-            <h4>Dumplings</h4>
-            <p>$10.00</p>
-            <a href="cart.php" class="btn">Add to cart &#8594;</a>
-        </div>
-        <div class="col-4">
-            <img src="images/7.png">
-            <h4>Tikka masala</h4>
-            <p>$20.00</p>
-            <a href="cart.php" class="btn">Add to cart &#8594;</a>
-        </div>
-        <div class="col-4">
-            <img src="images/8.png">
-            <h4>Gulab jamun Mix</h4>
-            <p>$15.00</p>
-            <a href="cart.php" class="btn">Add to cart &#8594;</a>
-        </div>
-        <div class="col-4">
-            <img src="images/9.png">
-            <h4>Gulab jamun Mix</h4>
-            <p>$15.00</p>
-            <a href="cart.php" class="btn">Add to cart &#8594;</a>
-        </div>
-        <div class="col-4">
-            <img src="images/10.png">
-            <h4>Gulab jamun Mix</h4>
-            <p>$15.00</p>
-            <a href="cart.php" class="btn">Add to cart &#8594;</a>
-        </div>
-        <div class="col-4">
-            <img src="images/11.png">
-            <h4>Gulab jamun Mix</h4>
-            <p>$15.00</p>
-            <a href="cart.php" class="btn">Add to cart &#8594;</a>
-        </div>
-        <div class="col-4">
-            <img src="images/12.png">
-            <h4>Gulab jamun Mix</h4>
-            <p>$15.00</p>
-            <a href="cart.php" class="btn">Add to cart &#8594;</a>
-        </div>
-    </div>
+
+
+?>
+<div>
+
 </div>
-<!-- brand logo-->
-<div class="brands">
-    <div class="small-container">
-        <div class="row">
-            <div class="col-5">
-                <img src="images/Heinz.png">
-            </div>
-            <div class="col-5">
-                <img src="images/equate.png">
-            </div>
-            <div class="col-5">
-                <img src="images/greatvalue.png">
-            </div>
-            <div class="col-5">
-                <img src="images/coca-cola.png">
-            </div>
-            <div class="col-5">
-                <img src="images/PayPal.png">
-            </div>
-        </div>
-    </div>
-</div>
+
 <footer class="footer">
     <div class="container">
         <div class="row">
